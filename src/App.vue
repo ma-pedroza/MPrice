698c6479a73960abfd08c5acce18f0f627527a19
Matheus Pedroza <matheusgomeszpedroza@gmail.com>
2026-05-22 12:17:44 -0300

    feat: add Tailwind

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <HelloWorld />
+  <div
+    class="bg-zinc-900 min-h-screen text-white flex items-center justify-center"
+  >
+    <h1 class="text-4xl font-bold text-red-400">Tailwind funcionando 🚀</h1>
+  </div>
 </template>
+
+<script setup>
+</script>
